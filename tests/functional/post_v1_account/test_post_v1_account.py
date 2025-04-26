eb863336a340e594efd4a3c83995eb3d98aafb16
Ruslan <SolovyovRI@yandex.ru>
2025-04-26 15:35:08 +0300

    test_post_v1_account passed

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -5,15 +5,17 @@ from json import loads
 from dm_api_account.apis.account_api import AccountApi
 from dm_api_account.apis.login_api import LoginApi
 from api_mailhog.apis.mailhog_api import MailhogApi
+from faker import Faker
 
-
+fake = Faker(['en-US'])
 def test_post_v1_account():
     account_api = AccountApi(host='http://5.63.153.31:5051')
     login_api = LoginApi(host='http://5.63.153.31:5051')
-    mailhog_api = MailhogApi(host='http://5.63..153.31:5025')
+    mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
     # регистрация пользователя
-    login = "vmenshikov_test456"
-    email = f"{login}@mail.ru"
+
+    login = fake.first_name() + fake.last_name()
+    email = fake.free_email()
     password = "123456789"
     json_data = {
         "login": login,
@@ -30,7 +32,7 @@ def test_post_v1_account():
     print(response.status_code)
     print(response.text)
     pprint.pprint(response.json)
-    assert responce.status_code == 200, f"Status code: {responce.status_code}"
+    assert responce.status_code == 201, f"Status code: {responce.status_code}"
 
     token = get_activation_token_by_login(login, response)
     assert token is not None, f"Token not found"
@@ -38,7 +40,7 @@ def test_post_v1_account():
     response = account_api.put_v1_account_token(token=token)
     print(response.status_code)
     print(response.text)
-    assert responce.status_code == 200, f"user not activated: {responce.status_code}"
+    assert responce.status_code == 201, f"user not activated: {responce.status_code}"
     # авторизоваться
     json_data = {
         'login': login,
@@ -48,28 +50,32 @@ def test_post_v1_account():
     response = login_api.post_v1_account_login(json_data=json_data)
     print(response.status_code)
     print(response.text)
-    assert responce.status_code == 200, f"user can't authorize: {responce.status_code}"
+    assert responce.status_code == 201, f"user can't authorize: {responce.status_code}"
 
 
-
-
-
-
-
-
-def get_activation_token_by_login(login, response):
+def get_activation_token_by_login(
+        login,
+        response
+        ):
     token = None
-    # получить активационный токен
     for item in response.json()['items']:
-        user_data = loads(item["Content"]["Body"])
-        user_login = user_data["login"]
-        # получаем токен из словаря
-        if user_login == login:
-            print(user_login)
-            token = user_data["ConfirmationLinkUrl"].split("/")[-1]
-            print(token)
-    return token
+        body = item['Content']['Body']
+        try:
+            user_data = loads(body)
+            user_login = user_data.get('Login')
+            if user_login == login:
+                token = user_data['ConfirmationLinkUrl'].split('/')[-1]
+                break
+        except ValueError:
+            for line in body.splitlines():
+                if 'ConfirmationLinkUrl' in line:
+                    token = line.split('/')[-1].strip()
+                    break
 
+    if not token:
+        raise ValueError(f'Не удалось найти токен активации для пользователя {login}')
+
+    return token
 
 
 
